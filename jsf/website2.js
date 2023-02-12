03ed1a9441f34eff93d0d7cce960024f38b96f32
avi1998s <avi6190i@gmail.com>
2023-02-12 14:38:43 +0200

    .

--- jsf/website2.js
+++ jsf/website2.js
@@ -1,4 +1,6 @@
-let input = document.querySelector("#inp2")
+let input = document.getElementById("p1")
 document.getElementById("but1").onclick=function (){
-    input.value = "bfggvsd"
+    input.innerHTML =parseInt(document.getElementById("inp1").value) + 
+    document.getElementById("inp2").value +
+    parseInt(document.getElementById("inp3").value) 
 } 
