80cd89c1341ff2f1a23ff443cc36023054710711
avi1998s <avi6190i@gmail.com>
2023-02-21 17:19:36 +0200

    finish all

--- jsf/colors.js
+++ jsf/colors.js
@@ -1,13 +1,10 @@
 let r = document.querySelector(":root");
-let colorOBG1 = ["#fe0eba","#ffffff","#155dda","#000000","#13e921","#e91313"]
-function set_color (){
-    for(i=0;i<colorOBG1.length;i++){
-    setInterval(() =>{r.style.setProperty("--color",colorOBG1[i])
-    },500)}
+let colorOBG1 = ["#fe0eba", "#ffffff", "#155dda", "#000000", "#13e921", "#e91313"]
+function set_color() {
+    let i = 0;
+    setInterval(() => {
+        r.style.setProperty("--color", colorOBG1[i])
+        i = (i + 1) % colorOBG1.length
+    }, 1000)
 }
-function myfor (varableOF){
-    for(i=0;i<varableOF.length;i++){
-       varableOF = varableOF[i]
-       alert(varableOF)
-    }
-}
+
