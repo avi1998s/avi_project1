7706620e53ffa2e78371b1b0c41351f354995ae6
avi1998s <avi6190i@gmail.com>
2023-02-21 21:26:26 +0200

    .

--- jsf/avi.js
+++ jsf/avi.js
@@ -1,3 +1,30 @@
+const
+container =document.querySelector("#container2"),
+h1 = document.createElement("h1"),
+p1 = document.createElement("p");
+container.appendChild(h1)
+container.appendChild(p1)
+h1.innerHTML= "ברוכים הבאים וכו"
+p1.innerHTML = "זה האתר החדש שלי כאן נתרגל כל מיני דברים מעניינים בנושא בנית אתרים"
+
+const
+container3 =document.querySelector("#container3"),
+table = document.createElement(table),
+th = document.createElement(th);
+container3.appendChild(table)
+table.appendChild(th)
+th.innerHTML="dfxdghgfh"
+
+
+
+
+
+
+
+
+
+
+
 
 //git config --global user.name "avi1998s"
 //git config --global user.email "avi6190i@gmail.com"
