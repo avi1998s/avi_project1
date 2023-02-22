748a7c934577f2ca324dc54ec6e425234a417e5e
avi1998s <avi6190i@gmail.com>
2023-02-22 17:02:14 +0200

    f

--- jsf/avi.js
+++ jsf/avi.js
@@ -4,20 +4,29 @@ h1 = document.createElement("h1"),
 p1 = document.createElement("p");
 container.appendChild(h1)
 container.appendChild(p1)
+h1.id="h1"
+p1.id="p1"
+
 h1.innerHTML= "ברוכים הבאים וכו"
 p1.innerHTML = "זה האתר החדש שלי כאן נתרגל כל מיני דברים מעניינים בנושא בנית אתרים"
 
+// יצירת טבלה באמצעות לולאות פור
+
 const
-container3 =document.querySelector("#container3"),
-table = document.createElement(table),
-th = document.createElement(th);
-container3.appendChild(table)
+names = ["avi sason","susu","dudu","shamby","tovi","baruch","shawarem","nachum",]
+table =document.querySelector("#table1"),
+th = document.createElement("th");
 table.appendChild(th)
-th.innerHTML="dfxdghgfh"
-
-
-
-
+th.innerHTML="הטבלה של אבי"
+for(let i=0;i<3;i++){
+    let trEL = document.createElement("tr");
+    table.appendChild(trEL)
+    for(let j=0;j<names.length;j++){
+        let tdEL = document.createElement("td");
+        trEL.appendChild(tdEL)
+        tdEL.innerHTML=names[j]
+    }
+}
 
 
 
