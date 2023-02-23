4bfcf3a14f610f5c755fdd5b19f9bff1606ad80a
avi1998s <avi6190i@gmail.com>
2023-02-23 13:11:17 +0200

    .

--- jsf/website2.js
+++ jsf/website2.js
@@ -1,8 +1,20 @@
-const root = document.documentElement;
-const button = document.getElementById('change-color-btn');
-button.addEventListener('click', () => {
-  setInterval(() => {
-    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-    root.style.setProperty('--color', randomColor);
-  }, 1000);
-});
+let names= ["nadav", "maya", "yonatan", "avi", "yossi", "bat el"],
+names1 = names.toString().toUpperCase().split(",");
+for(i of names1){
+    if (i.length>3 && i.length<7){
+        console.log("names of betwen 3-7 length is: " + i);
+    }
+
+    if(i.toString().includes(" ")){
+        let a = i.split(" ")
+        console.log("the position of two names is: " + i.indexOf());
+        console.log("first name: " + a[0])
+        console.log("last name num 2: " + i.slice(i.indexOf(" "),i.length));
+        console.log("last name num 2: " + a[a.length-1])
+    }
+}
+console.log("");
+
+for(i =1;i< names1.length+1;i++){
+    console.log(names1[i-1].toString().concat(" *".repeat(i)));
+}
